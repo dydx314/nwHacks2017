0f6075b67a999946493c20d9eca630a2bc8c1572
Henry Chee <mr.pointless@gmail.com>
2017-03-19 09:50:58 +0000

    Allow cross site

--- server/server.js
+++ server/server.js
@@ -17,10 +17,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.json({ message: 'Yo!'});
 });
 
 app.get('/highscore', function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
   pg.connect(config, function (err, client, done) {
     if (err) {
       res.json({ err: "Cannot connect"});
@@ -39,6 +41,7 @@ app.get('/highscore', function (req, res) {
 });
 
 app.post('/submit_score', function (req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
   if (!req.body["name"] || !req.body["score"]) {
     res.json({ err: "missing name or score"});
     return;
